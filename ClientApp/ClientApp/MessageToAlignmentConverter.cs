c460af0f1d454f8d7426119de1f1e6317ad04a85
zenonet <78077158+zenonet@users.noreply.github.com>
2024-08-02 17:45:00 +0200

    Significantly improved looks of chat messages

---
+++ ClientApp/ClientApp/MessageToAlignmentConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Layout;
+using Common;
+
+namespace ClientApp;
+
+public class MessageToAlignmentConverter : IValueConverter
+{
+    public static readonly MessageToAlignmentConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Message msg && targetType.IsAssignableTo(typeof(HorizontalAlignment)))
+        {
+            if (App.Client == null)
+            {
+                // If this is a test environment, make every message of a user with an even id right aligned
+                return msg.SenderId % 2 == 0 ? HorizontalAlignment.Right : HorizontalAlignment.Left;
+            }
+            return msg.SenderName == App.Client.Username ? HorizontalAlignment.Right : HorizontalAlignment.Left;
+        }
+        // converter used for the wrong type
+        return new BindingNotification(new InvalidCastException(), 
+            BindingErrorType.Error);
+    }
+
+    public object ConvertBack(object? value, Type targetType, 
+        object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ ClientApp/ClientApp/Views/MessageView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace ClientApp.Views;
+
+public partial class MessageView : UserControl
+{
+    public MessageView()
+    {
+        InitializeComponent();
+    }
+}
